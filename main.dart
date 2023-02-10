cfbde3d170ae9298e73a70aa7817ec58e4408013
koldin108902 <alex108902@naver.com>
2023-02-10 11:59:31 +0900

    add: data type

--- main.dart
+++ main.dart
@@ -19,3 +19,39 @@ void vaiable() {
   late final String later; // 초기 데이터 없이 변수 생성을 가능토록 함.
   later = "late";
 }
+
+void dataType() {
+  String a = "";
+  bool b = false;
+
+  int c = 12;
+  double d = 12.12;
+  num cd = 123.21; // int 와 double 모두 될 수 있음
+
+  // 위의 변수들은 Object를 기반으로 짜임
+
+  List list = [];
+  var list2 = [];
+
+  var item = [
+    1,
+    2,
+    3,
+    if (true) 10, // list를 init 할 때 if문 사용 가능.
+  ];
+
+  String str = "${item} is item";
+
+  for (var e in item) {
+    print(e);
+  }
+
+  Map<int, String> map = { // obj라고 생각
+    1: "",
+    2: ""
+  }
+
+  var set1 = {};
+  var list1 = [];
+  Set<String> set2 = {}; // 기본적으로 list와 같으나 모든 요소에 unique를 적용
+}
