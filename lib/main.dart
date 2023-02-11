79352c96c85c7a4333b6f55418e5c006bef3afa5
koldin108902 <alex108902@naver.com>
2023-02-11 23:35:05 +0900

    add: 잔액 - nomard made

--- lib/main.dart
+++ lib/main.dart
@@ -8,123 +8,80 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFF181818),
-        body: Padding(
-          padding: EdgeInsets.all(15),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                    Text(
-                      "hey, selena",
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                  Text(
+                    "hey, selena",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800),
+                  ),
+                  Text(
+                    "Welcome back",
+                    style: TextStyle(color: Colors.white.withOpacity(0.5)),
+                  ),
+                ])
+              ],
+            ),
+            SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total Balance',
+              style:
+                  TextStyle(fontSize: 22, color: Colors.white.withOpacity(0.8)),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              '\$5 194 482',
+              style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 50,
+                    ),
+                    child: Text(
+                      'Transfer',
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w800),
-                    ),
-                    Text(
-                      "Welcome back",
-                      style: TextStyle(color: Colors.white.withOpacity(0.5)),
-                    ),
-                  ])
-                ],
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          "Total Balance",
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.6),
-                            fontSize: 27,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        "\$5 194 382",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 50,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    height: 65,
-                    width: 170,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          "Transfer",
-                          style: TextStyle(color: Colors.black, fontSize: 18),
-                        ),
-                      ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          Color(0xFFF2B33A),
-                        ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50)),
-                        ),
+                        fontSize: 20,
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 65,
-                    width: 170,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          "Request",
-                          style:
-                              TextStyle(color: Color(0xFFFAFAFA), fontSize: 18),
-                        ),
-                      ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          Color.fromARGB(255, 31, 31, 31),
-                        ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50)),
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
-    );
+    ));
   }
 }
