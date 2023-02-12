5b04aff162e87fc89d4dc77e5f0f326baefaf982
koldin108902 <alex108902@naver.com>
2023-02-13 00:02:34 +0900

    update: add icon in card

--- lib/widgets/Cards.dart
+++ lib/widgets/Cards.dart
@@ -8,48 +8,63 @@ class Cards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
           color: const Color(0xFF1F2123),
           borderRadius: BorderRadius.circular(25)),
       child: Padding(
         padding: const EdgeInsets.all(30),
-        child: Row(children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Euro",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const Text(
-                    "6 428",
-                    style: TextStyle(
-                      fontSize: 20,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Euro",
+                  style: TextStyle(
                       color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      "6 428",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    'EUR',
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(0.8), fontSize: 20),
-                  )
-                ],
-              )
-            ],
-          )
-        ]),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'EUR',
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(0.8), fontSize: 20),
+                    )
+                  ],
+                )
+              ],
+            ),
+            Transform.scale(
+              scale: 2.2,
+              child: Transform.translate(
+                offset: const Offset(-5, 12),
+                child: const Icon(
+                  Icons.euro_rounded,
+                  color: Colors.white,
+                  size: 100,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
