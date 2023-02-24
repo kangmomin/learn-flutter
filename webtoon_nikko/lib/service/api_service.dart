3932dd5718c4769ac815f0297e00cbabfbc818e4
koldin108902 <alex108902@naver.com>
2023-02-24 23:27:33 +0900

    update: json string parse

--- webtoon_nikko/lib/service/api_service.dart
+++ webtoon_nikko/lib/service/api_service.dart
@@ -1,16 +1,23 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:webtoon_nikko/models/webtoon_model.dart';
 
 class ApiService {
   final String basicUtl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoons = [];
     final url = Uri.parse('$basicUtl/$today');
     var res = await http.get(url);
 
     if (res.statusCode == 200) {
-      print(res.body);
-      return;
+      final List<dynamic> webtoonsData = jsonDecode(res.body);
+      for (var e in webtoonsData) {
+        webtoons.add(WebtoonModel.fromJson(e));
+      }
+      return webtoons;
     }
 
     throw Error();
